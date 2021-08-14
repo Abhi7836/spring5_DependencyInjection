92799f607c14316e41b4ad1329ba12b75162027b
Sugali Abhishek <sugaliabhishek@197NODMB28412.local>
2021-08-14 19:40:57 +0530

    spring-context

--- src/main/java/abhi/springudemy/spring5di/Spring5DiApplication.java
+++ src/main/java/abhi/springudemy/spring5di/Spring5DiApplication.java
@@ -1,13 +1,18 @@
 package abhi.springudemy.spring5di;
 
+import abhi.springudemy.spring5di.controllers.MyController;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class Spring5DiApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Spring5DiApplication.class, args);
+        ApplicationContext ctx = SpringApplication.run(Spring5DiApplication.class, args);
+        MyController myController=(MyController) ctx.getBean("myController");
+        String greeting = myController.sayHello();
+        System.out.println(greeting);
     }
 
 }
---
+++ src/main/java/abhi/springudemy/spring5di/controllers/MyController.java
@@ -0,0 +1,11 @@
+package abhi.springudemy.spring5di.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MyController {
+    public String sayHello(){
+        System.out.println("Hello World");
+        return "Hi folks";
+    }
+}
